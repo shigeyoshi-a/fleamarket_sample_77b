d270cb4dcb0214570958d974a46fda85dfb1cdf4
ryoumashimo <ryoumashimo0711@gmail.comm>
2020-08-30 23:41:35 +0900

    Update new.js

--- app/assets/javascripts/items/new.js
+++ app/assets/javascripts/items/new.js
@@ -28,17 +28,18 @@ $(document).on('turbolinks:load', ()=> {
   });
 });
 
-$('#price_calc').on('input', function(){
-  var data = $('#price_calc').val();
-  var profit = Math.round(data * 0.9)
-  var fee = (data - profit)
-  console.log("a");
-  $('.charge-line_right').html(fee)
-  $('.charge-line_right').prepend('¥')
-  $('.profit-line_right').html(profit)
-  $('.profit-line_right').prepend('¥')
-  if(profit == '') {
-  $('.charge-line_right').html('');
-  $('.profit-line_right').html('');
-  }
+$(function(){
+  $('#price_calc').on('keyup', function(){
+    var data = $('#price_calc').val();
+    var profit = Math.round(data * 0.9)
+    var fee = (data - profit)
+    $('.charge-line_right').html(fee)
+    $('.charge-line_right').prepend('¥')
+    $('.profit-line_right').html(profit)
+    $('.profit-line_right').prepend('¥')
+    if(profit == '') {
+    $('.charge-line_right').html('ー');
+    $('.profit-line_right').html('ー');
+    }
+  })
 })
