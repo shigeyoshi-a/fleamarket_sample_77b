b70f15d17f6c66d380d3edd4a325ad30d3e416a6
noriya6335 <nonono6335@gmail.com>
2020-09-01 10:53:02 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :comments
+  has_many :items
   has_one :address
+  has_one :card
   
   validates :nickname, :family_name, :first_name, :family_name_reading, :first_name_reading, :birthday ,presence: true
   validates :family_name,
