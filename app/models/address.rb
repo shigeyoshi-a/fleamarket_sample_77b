5aea963d5c7db1621f7b2caa2e30b925e6b51796
noriya6335 <nonono6335@gmail.com>
2020-09-03 16:02:59 +0900

    validation_fix

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,17 +3,20 @@ class Address < ApplicationRecord
   validates :post_code ,:receiving_family_name,:receiving_first_name ,:receiving_family_name_reading,:receiving_first_name_reading,  
   :prefecture_id , :municipality ,:street_number , presence: true
 
+  full_width = {with: /\A[ぁ-んァ-ン一-龥]/, message: 'を全角で入力してください' }
+  full_width_kana = { with: /\A([ァ-ン]|ー)+\z/, message: 'を全角カタカナで入力してください' }
+  
   validates :post_code, length: { is: 7 }
   validates :post_code,
   format: { with: /\A\d{7}\z/, message: "をハイフン(-)無しで７桁の半角数字で入力してください" }
   validates :receiving_family_name,
-  format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'を全角で入力してください' }
+  format: full_width
   validates :receiving_first_name,
-  format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'を全角で入力してください' }
+  format: full_width
   validates :receiving_family_name_reading,
-  format: { with: /\A([ァ-ン]|ー)+\z/, message: 'を全角カタカナで入力してください' }
+  format: full_width_kana
   validates :receiving_first_name_reading,
-  format: { with: /\A([ァ-ン]|ー)+\z/, message: 'を全角カタカナで入力してください' }
+  format: full_width_kana
   validates :municipality,
-  format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'を全角で入力してください' }
+  format: full_width
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,16 @@ class User < ApplicationRecord
   has_one :address
   has_one :card
   
+  full_width = {with: /\A[ぁ-んァ-ン一-龥]/, message: 'を全角で入力してください' }
+  full_width_kana = { with: /\A([ァ-ン]|ー)+\z/, message: 'を全角カタカナで入力してください' }
+
   validates :nickname, :family_name, :first_name, :family_name_reading, :first_name_reading, :birthday ,presence: true
   validates :family_name,
-  format: {with: /\A[ぁ-んァ-ン一-龥]/, message: 'を全角で入力してください' }
+  format: full_width
   validates :first_name,
-  format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'を全角で入力してください' }
+  format: full_width
   validates :family_name_reading,
-  format: { with: /\A([ァ-ン]|ー)+\z/, message: 'を全角カタカナで入力してください' }
+  format: full_width_kana
   validates :first_name_reading,
-  format: { with: /\A([ァ-ン]|ー)+\z/, message: 'を全角カタカナで入力してください' }
+  format: full_width_kana
 end
