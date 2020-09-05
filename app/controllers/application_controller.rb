68589caf35a3d1b51a906d9d5f6df8af1597cdd7
ryoumashimo <ryoumashimo0711@gmail.comm>
2020-09-05 21:00:51 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth, if: :production?
- 
+  before_action :parents
+
+  def parents
+    @parents = Category.where(ancestry: nil)
+  end
+  
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name,:first_name,:family_name_reading,:first_name_reading,:birthday])
