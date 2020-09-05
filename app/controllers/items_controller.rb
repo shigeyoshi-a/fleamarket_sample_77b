feefdf1257669a53ea81829dbdbc09e7cfc1d8e4
ryoumashimo <ryoumashimo0711@gmail.comm>
2020-09-05 21:08:10 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,7 +26,7 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to root_path, notice: '商品を出品しました'
+      redirect_to root_path, notice: '商品を編集しました'
     else
       render :edit
     end
