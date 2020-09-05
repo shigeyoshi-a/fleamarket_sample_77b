fc00c5c9b164a50b68e5ee6eae6703874a46f246
ryoumashimo <ryoumashimo0711@gmail.comm>
2020-09-05 15:39:19 +0900

    Create item_spec.rb

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+describe Item do
+  describe '#create' do
+    it "商品名がないと出品できないこと" do
+    end
+
+    it "商品の説明がないと出品できないこと" do
+    end
+
+    it "カテゴリー情報がないと出品できないこと" do
+    end
+
+    it "ブランド名がなくても出品できること" do
+    end
+
+    it "商品の状態についての情報がないと出品できないこと" do
+    end
+
+    it "配送料の負担についての情報がないと出品できないこと" do
+    end
+
+    it "発送元の地域についての情報がないと出品できないこと" do
+    end
+
+    it "発送までの日数についての情報がないと出品できないこと" do
+    end
+
+    it "価格がないと出品できないこと" do
+    end
+
+    it "" do
+    end
+
+
+  end
+end
