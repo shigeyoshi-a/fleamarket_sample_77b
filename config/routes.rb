b18523fddcb0328a5f0762494598a3a33d1df1c6
noriya6335 <nonono6335@gmail.com>
2020-09-21 15:44:34 +0900

    category_page_fix

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
-  resources :users, only: :show
   resources :category, only: :show
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
