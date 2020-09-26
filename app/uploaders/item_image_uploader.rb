11ace09d5d42fb217d288d31fd1a78c4e49ae857
shigeyoshi-a <assapaamii@yahoo.co.jp>
2020-09-26 20:23:52 +0900

    Update item_image_uploader.rb

--- app/uploaders/item_image_uploader.rb
+++ app/uploaders/item_image_uploader.rb
@@ -3,7 +3,7 @@ class ItemImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
-  process resize_to_fit: [100, 100] 
+  process resize_to_fit: [600, 600] 
   
   # Choose what kind of storage to use for this uploader:
   if Rails.env.development? || Rails.env.test?
