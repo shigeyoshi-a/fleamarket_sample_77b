3b09f0c7c44f32c301b9ac2662aa44d7bb6c5cef
ryoumashimo <ryoumashimo0711@gmail.comm>
2020-10-03 20:35:53 +0900

    create set_item&category

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :update, :show, :destroy]
+
   def index
     @parents = Category.where(ancestry: nil)
     @newitems = Item.includes(:item_images).order("created_at DESC").limit(5)
@@ -34,7 +36,44 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    set_category
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to root_path, notice: '商品を編集しました'
+    else
+      set_category
+      render :edit
+    end
+  end
+
+  def show
+    @condition = @item.condition
+    @delivery_fee = @item.delivery_fee
+    @sending_area = @item.sending_area
+    @sending_days = @item.sending_days
+    @category = @item.category
+    @categories =  @category.items.order("created_at DESC").limit(3)
+  end
+
+  def destroy
+    @item.destroy
+
+    flash[:notice] = '商品を削除しました'
+    redirect_to root_path
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :brand, :condition_id, :delivery_fee_id, :sending_area_id, :sending_days_id, :price, item_images_attributes: [:image, :_destroy, :id]).merge(saler_id: current_user.id, user_id: current_user.id)
+  end
+
+  def set_item
     @item = Item.find(params[:id])
+  end
+
+  def set_category
     grandchild = @item.category
     child = grandchild.parent
 
@@ -52,54 +91,5 @@ class ItemsController < ApplicationController
     @grandchild_array = []
     @grandchild_array << child.name
     @grandchild_array << child.id
-  end
-
-  def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-      redirect_to root_path, notice: '商品を編集しました'
-    else
-      grandchild = @item.category
-      child = grandchild.parent
-
-      @category_parent_array = Category.where(ancestry: nil)
-      @parent_array = []
-      @parent_array << @item.category.parent.parent.name
-      @parent_array << @item.category.parent.parent.id
-
-      @category_children_array = Category.where(ancestry: child.ancestry)
-      @child_array = []
-      @child_array << child.name
-      @child_array << child.id
-
-      @category_grandchildren_array = Category.where(ancestry: grandchild.ancestry)
-      @grandchild_array = []
-      @grandchild_array << child.name
-      @grandchild_array << child.id
-      render :edit
-    end
-  end
-
-  def show
-    @item = Item.find(params[:id])
-    @condition = @item.condition
-    @delivery_fee = @item.delivery_fee
-    @sending_area = @item.sending_area
-    @sending_days = @item.sending_days
-    @category = @item.category
-    @categories =  @category.items.order("created_at DESC").limit(3)
-  end
-
-  def destroy
-    @item = Item.find(params[:id])
-    @item.destroy
-
-    flash[:notice] = '商品を削除しました'
-    redirect_to root_path
-  end
-
-  private
-  def item_params
-    params.require(:item).permit(:name, :description, :category_id, :brand, :condition_id, :delivery_fee_id, :sending_area_id, :sending_days_id, :price, item_images_attributes: [:image, :_destroy, :id]).merge(saler_id: current_user.id, user_id: current_user.id)
   end
 end
