6f66dc2cd3554e0f74b43701946dd2b37117d53e
noriya6335 <nonono6335@gmail.com>
2020-10-07 14:59:09 +0900

    Update registrations_controller.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,6 +12,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
+    if params[:sns_auth] == "true"
+      pass = Devise.friendly_token
+      params[:user][:password] = pass
+      params[:user][:password_confirmation] = pass
+    end
     params[:user][:birthday] = birthday_join
     @user = User.new(sign_up_params)
     unless @user.valid?
