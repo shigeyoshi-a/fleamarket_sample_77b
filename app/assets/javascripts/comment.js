d88704cf2423c23e32413d716e849216c2d79965
noriya6335 <nonono6335@gmail.com>
2020-10-13 14:38:07 +0900

    create_comment.js

---
+++ app/assets/javascripts/comment.js
@@ -0,0 +1,27 @@
+$(function(){
+  function buildHTML(comment){
+    let html = `
+                  ${comment.user_nickname}
+                  ${comment.comment}`
+    return html;
+  }
+  $('#new_comment').on('submit',function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data:formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.comment__list__main').append(html);
+      $('.textbox').val('');
+      $('.comment__submit__btn').prop("disabled", false);
+    })
+  })
+})
