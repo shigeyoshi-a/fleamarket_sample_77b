79e61ffb5155cddec8f7a9d56bf984b462c3c955
polpol1st <sendaizisinsokuhou@yahoo.co.jp>
2020-10-17 11:53:44 +0900

    recovery master

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -7,7 +7,7 @@ class PurchaseController < ApplicationController
     if card.blank?
       redirect_to new_card_path 
     else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_PRIVATE_KEY]
       customer = Payjp::Customer.retrieve(card.customer_id)
       @default_card_information = customer.cards.retrieve(card.card_id)
     end
@@ -15,7 +15,7 @@ class PurchaseController < ApplicationController
 
   def pay
     card = Card.where(user_id: current_user.id).first
-    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_PRIVATE_KEY]
     Payjp::Charge.create(
     :amount => @item.price,
     :customer => @card.customer_id,
